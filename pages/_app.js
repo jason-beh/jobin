7828f5c82bc64a77fcbbb1c52fc287968f04d256
Jason Beh <behjieshen@gmail.com>
2022-03-30 23:22:59 +1030

    Added meta description

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,18 @@
 import '../styles/globals.css';
 import { Provider } from 'next-auth/client';
 import Head from 'next/head';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    document.body.className = 'bg-gray-100';
+  });
+
+  const name = 'Jobin';
+  const description = 'Insert Description Here';
+  const url = 'https://jobin-my.vercel.app/';
+  const image = '/images/meta-image.png';
+
   return (
     <Provider
       session={pageProps.session}
@@ -13,6 +23,23 @@ export default function App({ Component, pageProps }) {
     >
       <Head>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
+        <meta name="title" content={name} />
+        <meta name="description" content={description} />
+        <link rel="icon" type="image/png" href="/images/favicon.png" />
+        <meta name="theme-color" content="#1F2937" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={name} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={image} />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content={url} />
+        <meta property="twitter:title" content={name} />
+        <meta property="twitter:description" content={description} />
+        <meta property="twitter:image" content={image} />
       </Head>
       <Component {...pageProps} />
     </Provider>
